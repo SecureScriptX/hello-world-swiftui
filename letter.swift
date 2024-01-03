060ead5a8cdb96bcc8adab769a54a495759336f8
print("Ryan Stewart") <152526975+SecureScriptX@users.noreply.github.com>
2024-01-02 21:10:58 -0500

    Create letter.swift
     let’s write a letter to your future self.
    
    In Letter.swift, let’s add the following:
    
        Goal(s) for yourself.
        Name and date.

---
+++ letter.swift
@@ -0,0 +1,3 @@
+print("1/2/2024 Dear self, I'm commited to putting in my best effort six days a week, consistently engaging in learning code. My goal is to create distinctive and enjoyable projects, showcasing my knowledge and skills in Swift iOS. Once confident in my abilities, the plan is to continue developing ideas and practicing through coding challenges. Best regards, Ryan")
+Output:
+1/2/2024 Dear self, I'm commited to putting in my best effort six days a week, consistently engaging in learning code. My goal is to create distinctive and enjoyable projects, showcasing my knowledge and skills in Swift iOS. Once confident in my abilities, the plan is to continue developing ideas and practicing through coding challenges. Best regards, Ryan
